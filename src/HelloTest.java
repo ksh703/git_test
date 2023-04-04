cadd3c48ca47ddde9f444e34cf16ad03640b7c8e
ksh <kj_ksh703@naver.com>
2023-04-04 17:11:34 +0900

    first commit

---
+++ src/HelloTest.java
@@ -0,0 +1,11 @@
+
+public class HelloTest {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello world");
+		System.out.println("Hello world2");
+		System.out.println("Hello world3");
+	}
+
+}
